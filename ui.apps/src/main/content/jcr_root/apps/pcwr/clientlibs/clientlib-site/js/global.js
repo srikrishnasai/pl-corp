3edaa7f274d1cd24c2ce78047b2da3690991cb6b
Mark Juliusson <mjuliusson@pacificlife.com>
2021-08-12 19:17:00 +0000

    Merged PR 8052: SEO fix (Lou)
    Related work items: #60511

--- ui.apps/src/main/content/jcr_root/apps/pcwr/clientlibs/clientlib-site/js/global.js
+++ ui.apps/src/main/content/jcr_root/apps/pcwr/clientlibs/clientlib-site/js/global.js
@@ -61,3 +61,34 @@ $(document).ready(function() {
       });
     }, false);
   })();
+
+
+//Default-passive-events: https://zzarcon.github.io/default-passive-events/
+!function (e) { "function" == typeof define && define.amd ? define(e) : e() }(function () { var e, t = ["scroll", "wheel", "touchstart", "touchmove", "touchenter", "touchend", "touchleave", "mouseout", "mouseleave", "mouseup", "mousedown", "mousemove", "mouseenter", "mousewheel", "mouseover"]; if (function () { var e = !1; try { var t = Object.defineProperty({}, "passive", { get: function () { e = !0 } }); window.addEventListener("test", null, t), window.removeEventListener("test", null, t) } catch (e) { } return e }()) { var n = EventTarget.prototype.addEventListener; e = n, EventTarget.prototype.addEventListener = function (n, o, r) { var i, s = "object" == typeof r && null !== r, u = s ? r.capture : r; (r = s ? function (e) { var t = Object.getOwnPropertyDescriptor(e, "passive"); return t && !0 !== t.writable && void 0 === t.set ? Object.assign({}, e) : e }(r) : {}).passive = void 0 !== (i = r.passive) ? i : -1 !== t.indexOf(n) && !0, r.capture = void 0 !== u && u, e.call(this, n, o, r) }, EventTarget.prototype.addEventListener._original = e } });
+//# sourceMappingURL=index.umd.js.map
+
+//Alt fix for default passive events
+/*
+
+jQuery.event.special.touchstart = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+};
+jQuery.event.special.touchmove = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+};
+jQuery.event.special.wheel = {
+    setup: function( _, ns, handle ){
+        this.addEventListener("wheel", handle, { passive: true });
+    }
+};
+jQuery.event.special.mousewheel = {
+    setup: function( _, ns, handle ){
+        this.addEventListener("mousewheel", handle, { passive: true });
+    }
+};
+
+*/
